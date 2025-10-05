6eaa8566182eca11a12742b1d6950cb2c14dad6c
brayden-s-haws <haws@hey.com>
2025-10-05 14:11:39 -0600

    Adding logout functionality

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # path('login/', views.login_view, name='login'),
-   #  path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
     ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import logout
 
 # Create your views here.
 def register_view(request):
         if request.method == 'POST':
-            form = UserCreationForm(request.POST)
+            form = RegisterForm(request.POST)
             if form.is_valid():
                 form.save()
-                return redirect('food:index')
-        form = UserCreationForm()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Account created for {username}')
+                return redirect('login')
+        form = RegisterForm()
         return render(request, 'users/register.html',{'form': form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
