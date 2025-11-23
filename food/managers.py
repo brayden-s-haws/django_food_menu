61995c689a642c09f664f706542529f0ea1f8be8
brayden-s-haws <haws@hey.com>
2025-11-23 14:51:17 -0700

    Add soft delete functionality to Item model
    - Introduced `is_deleted` and `deleted_at` fields in the `Item` model for soft delete.
    - Updated custom `delete` method to set soft delete flags instead of actual deletion.
    - Adjusted `ItemManager` to filter out soft-deleted items by default.
    - Added `all_objects` manager for accessing all items, including soft-deleted ones.

--- food/managers.py
+++ food/managers.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class ItemManager(models.Manager):
-    def cheap_items(self):
-        return self.filter(item_price__lt=5)
-    def expensive_items(self):
-        return self.filter(item_price__gt=5)
-    def search(self, keyword):
-        return self.filter(item_name__icontains=keyword)
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+    def deleted(self):
+        return super().get_queryset().filter(is_deleted=True)
--- food/models.py
+++ food/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 
 from food.managers import ItemManager
 
@@ -17,6 +18,11 @@ class Item(models.Model):
     def get_absolute_url(self):
         return reverse('food:index')
 
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
     user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200,db_index=True)
     item_desc = models.CharField()
@@ -24,8 +30,11 @@ class Item(models.Model):
     item_image = models.URLField(max_length=500,default='https://images.unsplash.com/vector-1750272454955-229749cf8e08?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2QlMjBpdGVtJTIwcGxhY2Vob2xkZXJ8ZW58MHx8MHx8fDA%3D')
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False) # soft delete flag
+    deleted_at = models.DateTimeField(blank=True, null=True) # saves timestamp of deletion
 
     objects = ItemManager()
+    all_objects = models.Manager()
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
