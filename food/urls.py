2e4c2203f01824287de4c15c5e7c97aa45f255b3
brayden-s-haws <haws@hey.com>
2025-12-04 19:39:05 -0700

    Cache index view for improved performance by applying `cache_page` decorator in `urls.py`.

--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
 
 app_name = 'food'
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',cache_page(60*15)(views.index),name='index'),
     path('<int:pk>/',views.FoodDetailView.as_view(),name='detail'),
     path('add/',views.CreateItemView.as_view(),name='add'),
     path('update/<int:pk>/',views.UpdateItemView.as_view(),name='update_item'),
